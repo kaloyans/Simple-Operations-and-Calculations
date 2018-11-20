26aa4dbb5b46498e3113a2354c77a3ff8e968ffe
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 17:25:56 +0200

    Create circleAreaAndPerimeter.cs

---
+++ circleAreaAndPerimeter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSyntaxDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+            double perimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine($"Area = {area}");
+            Console.WriteLine($"Perimeter = {perimeter}");
+
+        }
+    }
+}
