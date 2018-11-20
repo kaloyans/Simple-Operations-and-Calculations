a481c39dba92f89d76d3e34d310d8ae249c9606d
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 17:32:38 +0200

    Create usdToBgn.cs

---
+++ usdToBgn.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+                
+            Console.WriteLine(Math.Round(bgn, 2));
+
+        }
+    }
+}
