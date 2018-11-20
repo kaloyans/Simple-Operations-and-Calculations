908446c982cc1e596c53fa685cc79816ead79b51
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 17:31:20 +0200

    Create 2DRectangleArea.cs

---
+++ 2DRectangleArea.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double a = Math.Abs(x1 - x2);
+            double b = Math.Abs(y1 - y2);
+
+            double s = a * b;
+            double p = 2 * (a + b);
+            
+            Console.WriteLine(s);
+            Console.WriteLine(p);
+            
+
+
+        }
+    }
+}
