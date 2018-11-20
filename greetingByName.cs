1ecd2ef556557b6b776aa2b9ae1c829d7b585f22
Kaloyan Stankov <45176113+kaloyans@users.noreply.github.com>
2018-11-20 17:24:08 +0200

    Create greetingByName.cs

---
+++ greetingByName.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseSyntaxDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name + "!" );
+
+
+        }
+    }
+}
+  
